499eb5becc914ae15e40c3c06a3b49fb5fe8e92d
CruzDataConsulting <hdcb13@gmail.com>
2024-05-22 18:28:06 -0600

    rutina crea secrets

---
+++ crea_secrets.py
@@ -0,0 +1,12 @@
+import toml
+
+output_file=".streamlit/secrets.toml"
+
+with open("primera-base-5bb2f-firebase-adminsdk-2ch28-b147cbc93b.json") as json_file:
+    json_text=json_file.read()
+
+    config={"textkey":json_text}
+    toml_config=tomls.dumps(config)
+
+    with open(output_file,"w") as target:
+        target.write(toml_config)
